dcafd378c82e9619fde79331f1528136d71a9571
BalaDynamic01 <Baladynamic01@gmail.com>
2024-10-16 07:34:19 +0530

    modified changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 
+
+
+const Title = () => (
+      <h1 className="title">Nameste react ........</h1>
+)
+// const number = 10000
+
 const HeadingComponent = () =>(
      <div id="container">
+      <Title/>
       <h1 className="heading">Nameste React function component</h1>
      </div>
 )
